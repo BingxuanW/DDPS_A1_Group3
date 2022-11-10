0ea3f5ce5c1c280bfe94c7750d96caa64135c676
Bingxuan Wu <91846976+BingxuanW@users.noreply.github.com>
2022-11-11 00:45:50 +0100

    Update repeat_iteration_times_spark_hadoop.sh

--- repeat_iteration_times_spark_hadoop.sh
+++ repeat_iteration_times_spark_hadoop.sh
@@ -1,6 +1,8 @@
-# Configure /conf/workloads/ml/kmeans.conf large_max_iteration: 1,5
+#Configure /conf/workloads/ml/kmeans.conf hibench_large_max_iteration: 1 or 5
+
 /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/prepare/prepare.sh
 # Repeat spark
+/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
@@ -10,16 +12,17 @@
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
-#/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/spark/run.sh
+
+
 # Repeat Hadoop
+/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
 #/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-#/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
-/var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
+# /var/scratch/ddps2203/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh
 
